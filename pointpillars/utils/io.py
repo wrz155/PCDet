8da6dcd270c95fc448489c9dacc7fcb4ee98050c
wangruizhen <you@example.com>
2024-05-22 11:48:24 +0800

    add io.py

--- pointpillars/utils/io.py
+++ pointpillars/utils/io.py
@@ -5,3 +5,11 @@ import numpy as np
 
 def read_pickle(filepath, suffix=".pkl"):
     assert os.path.splitext(filepath)[1] == suffix
+    with open(filepath, 'rb') as f:
+        data = pickle.load(f)
+    return data
+
+
+def write_pickle(result, filepath):
+    with open(filepath, 'wb') as f:
+        pickle.dump(result, f)
